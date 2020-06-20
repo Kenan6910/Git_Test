9802a57308f4e091e7ec74665761df4c9e33cd82
Kenan6910 <67188285+Kenan6910@users.noreply.github.com>
2020-06-20 14:10:41 +0300

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,6 +2,7 @@ public class TestClass {
 
     public static void main(String[] args) {
         System.out.println("Git'ten Merhaba");
+         System.out.println("GitHup'tan Merhaba");
     }
 
 
